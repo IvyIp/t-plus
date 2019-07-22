e37c4f6cd3c6db9aa9ee5dc8b7fb604489994259
PYChen <gary.chen@kaiostech.com>
2019-07-22 12:05:08 +0800

    add log for insall app

--- js/app-child.js
+++ js/app-child.js
@@ -10,17 +10,20 @@ function init() {
   twitterBtn.focus();
   twitterBtn.addEventListener('keydown', (e)=>{
     let url = 'https://mobile.twitter.com/kaios.webapp';
-    keyCode.textContent = e.key;
     console.log('app url:' + url);
     if (navigator.mozApps) {
       var request = navigator.mozApps.install(url);
       request.onsuccess = function() {
         console.log('app ok');
+        keyCode.textContent = e.key + 'app ok';
       };
 
       request.onerror = function() {
         console.log('app error');
+        keyCode.textContent = e.key + 'app error';
       };
+    }else {
+      keyCode.textContent = e.key;
     }
   });
 
